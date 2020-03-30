0e76fc16f84b77be130cb6e0b82f843c251bbbc8
tikitavi <rtikitavi@gmail.com>
2020-03-30 17:06:41 +0300

    add contrail command charm
    Signed-off-by: tikitavi <rtikitavi@gmail.com>
    Closes-jira-bug: CEM-14224
    Change-Id: I137c89a20e7b1e1ae76e6514cf4972d9dd0ced4a

---
+++ contrail-command/hooks/contrail_command_hooks.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+import sys
+
+from charmhelpers.core.hookenv import (
+    Hooks,
+    UnregisteredHookError,
+    config,
+    log,
+    relation_get,
+    relation_set,
+    status_set,
+)
+
+import contrail_command_utils as utils
+import common_utils
+import docker_utils
+
+
+hooks = Hooks()
+config = config()
+
+
+@hooks.hook("install.real")
+def install():
+    status_set('maintenance', 'Installing...')
+
+    docker_utils.install()
+    utils.update_charm_status()
+
+
+@hooks.hook("config-changed")
+def config_changed():
+    docker_utils.config_changed()
+    utils.update_charm_status()
+
+
+@hooks.hook("update-status")
+def update_status():
+    utils.update_charm_status()
+
+
+@hooks.hook("upgrade-charm")
+def upgrade_charm():
+    utils.update_charm_status()
+
+
+@hooks.hook("contrail-controller-relation-joined")
+@hooks.hook("contrail-controller-relation-changed")
+def contrail_controller_changed():
+    settings = {'unit-type': 'command'}
+    relation_set(relation_settings=settings)
+
+    data = relation_get()
+    if "orchestrator-info" in data:
+        config["orchestrator_info"] = data["orchestrator-info"]
+    else:
+        config.pop("orchestrator_info", None)
+    config.save()
+
+    utils.update_charm_status(import_cluster=True)
+
+
+def main():
+    try:
+        hooks.execute(sys.argv)
+    except UnregisteredHookError as e:
+        log("Unknown hook {} - skipping.".format(e))
+
+
+if __name__ == "__main__":
+    main()
