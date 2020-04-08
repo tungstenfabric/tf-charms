5e8c2d69e379700e8fccaebd90f364a884207310
Andrey Pavlov <andrey-mp@yandex.ru>
2020-04-08 09:26:30 +0300

    generate repo-info for all charms
    Change-Id: I9a6218ccaecce72dad26256fadd007dfb12ac293

--- tools/generate-repo-info.sh
+++ tools/generate-repo-info.sh
@@ -8,6 +8,6 @@ function write_repo_info() {
   printf "commit-sha-1: $(git rev-parse HEAD)\ncommit-short: $(git rev-parse --short HEAD)\nbranch: $(git rev-parse --abbrev-ref HEAD)\nremote: $(git config --get remote.origin.url)\ninfo-generated: $(date -u)" > "$my_dir/../$folder/repo-info"
 }
 
-for folder in contrail-agent contrail-analytics contrail-analyticsdb contrail-controller contrail-keystone-auth contrail-openstack ; do
+for folder in contrail-agent contrail-analytics contrail-analyticsdb contrail-controller contrail-keystone-auth contrail-openstack contrail-kubernetes-master contrail-kubernetes-node ; do
   write_repo_info $folder
 done
