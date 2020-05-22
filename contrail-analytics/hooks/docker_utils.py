409ee6c8bcbce94afb920a8d4deba5acb069f40f
Arina Altynova <aaltynova@progmaticlab.com>
2020-05-22 17:53:05 +0300

    http & no_proxy are added to _format_curl_https_proxy_opt() function which is renamed to _format_curl_proxy_opt() partial-jira-bug:CEM-15402
    Change-Id: Ida7c695b29f233b30238383bb4a5b2e268a52183

--- contrail-analytics/hooks/docker_utils.py
+++ contrail-analytics/hooks/docker_utils.py
@@ -24,13 +24,23 @@ DOCKER_CLI = "/usr/bin/docker"
 DOCKER_COMPOSE_CLI = "docker-compose"
 
 
-def _format_curl_https_proxy_opt():
-    proxy_settings = env_proxy_settings(['https'])
+def _format_curl_proxy_opt():
+    proxy_settings = env_proxy_settings(['http','https','no_proxy'])
     https_proxy = None
+    http_proxy = None
+    no_proxy = None
+    str = ''
     if proxy_settings:
         https_proxy = proxy_settings.get('https_proxy')
-        return '--proxy {}'.format(https_proxy) if https_proxy else ''
-    return ''
+        if https_proxy:
+            str += '--proxy {} '.format(https_proxy)
+        http_proxy = proxy_settings.get('http_proxy')
+        if http_proxy:
+            str += '--proxy {} '.format(http_proxy)
+        no_proxy = proxy_settings.get('no_proxy')
+        if no_proxy:
+            str += '--noproxy {}'.format(proxy)
+    return str
 
 
 def install():
@@ -58,7 +68,7 @@ def install():
             "set -o pipefail ; curl {} "
             "-fsSL --connect-timeout 10 "
             "{} | sudo apt-key add -"
-            "".format(_format_curl_https_proxy_opt(), docker_key_url)
+            "".format(_format_curl_proxy_opt(), docker_key_url)
         ]
         check_output(cmd)
     arch = "amd64"
