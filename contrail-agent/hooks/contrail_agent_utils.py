28090c33c0d41023aa1a97462eb9fbe18a4cc927
Arina Argentova <aargentova@progmaticlab.com>
2020-10-05 21:19:16 +0300

    [+]   allow to disable optional groups - analytics-alarm, analytics-snmp  inside contrail-analytics and whole contrail-analyticsdb charm partial-jira-bug: CEM-19505
    Change-Id: I8f38b796c55d95b7bdf198d4957d0834a372b22f

--- contrail-agent/hooks/contrail_agent_utils.py
+++ contrail-agent/hooks/contrail_agent_utils.py
@@ -209,8 +209,9 @@ def get_context():
     ctx["controller_servers"] = common_utils.json_loads(config.get("controller_ips"), list())
     ctx["control_servers"] = common_utils.json_loads(config.get("controller_data_ips"), list())
     ctx["analytics_servers"] = common_utils.json_loads(config.get("analytics_servers"), list())
+    ctx["analyticsdb_enabled"] = config.get("analyticsdb_enabled")
     ctx["config_analytics_ssl_available"] = common_utils.is_config_analytics_ssl_available()
-
+    ctx["analyticsdb_enabled"] = config.get("analyticsdb_enabled")
     if "plugin-ips" in config:
         plugin_ips = common_utils.json_loads(config["plugin-ips"], dict())
         my_ip = unit_get("private-address")
