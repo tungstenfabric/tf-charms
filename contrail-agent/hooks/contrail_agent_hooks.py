2a5595e4427f6bdb6895b60327f07cc095d15ec4
Arina Argentova <aargentova@progmaticlab.com>
2020-11-13 14:30:53 +0300

    [~]  do not print RelData obj to prevent secret data leaks into contrail-agent and contrail-kubernetes-master juju logs
    partial-jira-bug: CEM-15938
    Change-Id: I50bd722b554826fb4e62ef50d9137e7f626bc0f6

--- contrail-agent/hooks/contrail_agent_hooks.py
+++ contrail-agent/hooks/contrail_agent_hooks.py
@@ -76,7 +76,6 @@ def contrail_controller_joined():
 @hooks.hook("contrail-controller-relation-changed")
 def contrail_controller_changed():
     data = relation_get()
-    log("RelData: " + str(data))
 
     def _update_config(key, data_key):
         if data_key in data:
