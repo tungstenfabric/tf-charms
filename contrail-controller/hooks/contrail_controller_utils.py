56b4281efb0cc2c5d2a3041904379824f60bfe93
Andrey Pavlov <andrey-mp@yandex.ru>
2020-11-28 08:33:35 +0300

    do not overwrite cert file if it's not changed
    let's check file hash before writing and do not write it
    it hash is the same
    
    closes-jira-bug: CEM-20332
    Change-Id: I40e9a18698ad8eb07b622d4d0c45eebaf65977ee

--- contrail-controller/hooks/contrail_controller_utils.py
+++ contrail-controller/hooks/contrail_controller_utils.py
@@ -197,9 +197,11 @@ def _update_charm_status(ctx, services_to_run=None):
         missing_relations.append("contrail-analytics")
     if common_utils.get_ip() not in ctx.get("controller_servers"):
         missing_relations.append("contrail-cluster")
+    if config.get('tls_present', False) != config.get('ssl_enabled', False):
+        missing_relations.append("tls-certificates")
     if missing_relations:
         status_set('blocked',
-                   'Missing relations: ' + ', '.join(missing_relations))
+                   'Missing or incomplete relations: ' + ', '.join(missing_relations))
         return
     if len(ctx.get("controller_servers")) < config.get("min-cluster-size"):
         status_set('blocked',
