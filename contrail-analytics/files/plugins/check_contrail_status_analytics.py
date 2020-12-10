22078ab382073f6c78dd4629ad57bb295926e53d
tikitavi <rtikitavi@gmail.com>
2020-12-10 12:31:30 +0300

    Use json format to call contrail-status
    cherry-pick from master
    
    Signed-off-by: tikitavi <rtikitavi@gmail.com>
    Change-Id: I1a879d9c34da60cfe9511cbdb9adb09d206a23f7
    Closes-Jira-bug: CEM-20640

--- contrail-analytics/files/plugins/check_contrail_status_analytics.py
+++ contrail-analytics/files/plugins/check_contrail_status_analytics.py
@@ -2,9 +2,10 @@
 
 import subprocess
 import sys
+import json
 
 SERVICES = {
-    '5.0': {
+    500: {
         "analytics": [
             "snmp-collector",
             "query-engine",
@@ -15,7 +16,7 @@ SERVICES = {
             "topology",
         ]
     },
-    '5.1': {
+    9999: {
         "analytics": [
             "api",
             "nodemgr",
@@ -38,8 +39,7 @@ WARNING = 1
 CRITICAL = 2
 
 
-def check_contrail_status(services):
-
+def get_contrail_status_txt(services):
     try:
         output = subprocess.check_output("export CONTRAIL_STATUS_CONTAINER_NAME=contrail-status-analytics-nrpe ; sudo -E contrail-status", shell=True).decode('UTF-8')
     except subprocess.CalledProcessError as err:
@@ -61,8 +61,31 @@ def check_contrail_status(services):
             continue
         if group and len(words) >= 2 and group in services:
             srv = words[0].split(':')[0]
-            statuses.setdefault(group, dict())[srv] = (
-                words[1], ' '.join(words[2:]))
+            statuses.setdefault(group, list()).append(
+                {srv: ' '.join(words[1:])})
+
+    return statuses
+
+
+def get_contrail_status_json(services):
+    try:
+        output = json.loads(subprocess.check_output("export CONTRAIL_STATUS_CONTAINER_NAME=contrail-status-analytics-nrpe ; sudo -E contrail-status --format json", shell=True).decode('UTF-8'))
+    except subprocess.CalledProcessError as err:
+        message = ('CRITICAL: Could not get contrail-status.'
+                   ' return code: {} cmd: {} output: {}'.
+                   format(err.returncode, err.cmd, err.output))
+        print(message)
+        sys.exit(CRITICAL)
+
+    statuses = output["pods"]
+    return statuses
+
+
+def check_contrail_status(services, version=None):
+    if version > 1912:
+        statuses = get_contrail_status_json(services)
+    else:
+        statuses = get_contrail_status_txt(services)
 
     for group in services:
         if group not in statuses:
@@ -71,15 +94,15 @@ def check_contrail_status(services):
             print(message)
             sys.exit(WARNING)
         for srv in services[group]:
-            if srv not in statuses[group]:
+            if not any(srv in key for key in statuses[group]):
                 message = ('WARNING: {} is absent in the contrail-status'
                            .format(srv))
                 print(message)
                 sys.exit(WARNING)
-            status, desc = statuses[group].get(srv)
+            status = next(stat[srv] for stat in statuses[group] if srv in stat)
             if status not in ['active', 'backup']:
                 message = ('CRITICAL: {} is not ready. Reason: {}'
-                           .format(srv, desc if desc else status))
+                           .format(srv, status))
                 print(message)
                 sys.exit(CRITICAL)
     print('Contrail status OK')
@@ -88,4 +111,17 @@ def check_contrail_status(services):
 
 if __name__ == '__main__':
     cver = sys.argv[1]
-    check_contrail_status(SERVICES[cver])
+    if '.' in str(cver):
+        if cver == '5.0':
+            version = 500
+        elif cver == '5.1':
+            version = 510
+        else:
+            print("CRITICAL: invalid version: {}".format(cver))
+            sys.exit(CRITICAL)
+    elif not cver.isdigit():
+        print("CRITICAL: invalid version: {}".format(cver))
+        sys.exit(CRITICAL)
+    else:
+        version = int(cver)
+    check_contrail_status(SERVICES.get(version, SERVICES.get(9999)), version=version)
