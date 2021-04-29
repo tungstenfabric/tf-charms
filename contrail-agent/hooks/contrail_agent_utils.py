317a4e2288c5f97374ef96f940c35d886be9b4d6
Andrey Pavlov <andrey-mp@yandex.ru>
2021-04-29 13:17:46 +0300

    add XFLOW_NODE_IP to contrail-agent
    closes-jira-bug: CEM-21641
    Change-Id: Ib095902159f40d057a3b37dc3073080f657d94cd

--- contrail-agent/hooks/contrail_agent_utils.py
+++ contrail-agent/hooks/contrail_agent_utils.py
@@ -178,6 +178,7 @@ def get_context():
     ctx["sriov_physical_interface"] = config.get("sriov-physical-interface")
     ctx["sriov_numvfs"] = config.get("sriov-numvfs")
     ctx["max_vm_flows"] = config.get("max-vm-flows")
+    ctx["xflow_node_ip"] = config.get("xflow-node-ip")
     ctx["contrail_version"] = common_utils.get_contrail_version()
 
     # NOTE: charm should set non-fqdn hostname to be compatible with R5.0 deployments
