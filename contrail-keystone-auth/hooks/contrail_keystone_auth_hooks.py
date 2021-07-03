94858fc68cc85cc0bbe376f5571f8eeb09fbd6ff
Andrey Pavlov <andrey-mp@yandex.ru>
2021-07-03 19:36:39 +0300

    Support interaction with multi-region Keystone deployment
    In OpenStack deployment with multi-regions Keystone a charm-keystone
    communicates attribute 'service_region' as part of relation identity-admin
    with contrail-keystone-auth as a space separated list of defined regions.
    The value of service_region attribute is then passed to contrail-control
    via auth_info['keystone_region'], since contrail-control expects exactly
    one OpenStack region name and fails to handle a string of space separated
    regions. Contrail works with single OpenStack region, thus in multi-region
    Keystone there have to be a way to specify which region Contrail has to
    operate in.
    This commit introduces new config option 'region' and set of code
    changes to match region against list of regions in service_region
    and adjust auth_info['keystone_region'] accordingly.
    
    Change-Id: Ibd286463e66b860e80dcb727018096bffdb53abb

--- contrail-keystone-auth/hooks/contrail_keystone_auth_hooks.py
+++ contrail-keystone-auth/hooks/contrail_keystone_auth_hooks.py
@@ -34,6 +34,32 @@ def _decode_cert(key):
     return None
 
 
+def _match_region(region, service_region):
+    """Match configured region against keystone service_region value obtained
+    via identity-admin relation from keystone. If keystone is configured
+    with multiple regions then value of service_region is a space-separated
+    string of keystone region names. If the region can be found
+    among regions in the service_region then return the it.
+    If no match is found between non-empty configured region and the service_region
+    then exception is raised - user has to choose correct value.
+    """
+    service_regions = service_region.split()
+    if not region:
+        if len(service_regions) > 1:
+            log("Region is not set in config. Keystone has multiple values {}"
+                "".format(service_region), level=ERROR)
+            raise("Keystone provided more than 1 region. Please configure region in config.")
+        # otherwise return original string from keystone even if it's empty
+        return service_region
+
+    if region not in service_regions:
+        log("Region in config({}) is not equal to keystone provided({})"
+            "".format(region, service_region), level=ERROR)
+        raise("Configured region is not present in keystone. Please change it or unset.")
+
+    return region
+
+
 def update_relations(rid=None):
     rids = [rid] if rid else relation_ids("contrail-auth")
     if not rids:
@@ -43,6 +69,7 @@ def update_relations(rid=None):
     if auth_info:
         data = json.loads(auth_info)
         data["keystone_ssl_ca"] = _decode_cert("ssl_ca")
+        data["keystone_region"] = _match_region(config.get("region"), config.get("service_region"))
         auth_info = json.dumps(data)
     settings = {
         "auth-info": auth_info
@@ -78,7 +105,6 @@ def identity_admin_changed():
             "keystone_admin_user": relation_get("service_username"),
             "keystone_admin_password": relation_get("service_password"),
             "keystone_admin_tenant": relation_get("service_tenant_name"),
-            "keystone_region": relation_get("service_region"),
             "keystone_api_version": api_version,
             "keystone_api_suffix": api_suffix,
             "keystone_api_tokens": api_tokens,
@@ -91,8 +117,11 @@ def identity_admin_changed():
         }
         auth_info = json.dumps(auth_info)
         config["auth_info"] = auth_info
+        # save list of regions in case of multi-region setup
+        config["service_region"] = relation_get("service_region")
     else:
         config.pop("auth_info", None)
+        config.pop("service_region")
 
     update_relations()
     update_status()
@@ -106,6 +135,7 @@ def identity_admin_departed():
     if count > 0:
         return
     config.pop("auth_info", None)
+    config.pop("service_region")
 
     update_relations()
     update_status()
