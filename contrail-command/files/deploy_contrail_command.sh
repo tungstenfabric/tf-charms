78a85a0b66ee8b65e6ef3b7bd4a8d48ed8606f1f
Andrey Pavlov <andrey-mp@yandex.ru>
2021-11-10 21:41:03 +0300

    export locale to run pip install
    otherwise it may fail with non-ascii character
    closes-jira-bug: CEM-24717
    
    Change-Id: Ie6313b399d0cc476ffda4aaffb70221cbd1b60bc

--- contrail-command/files/deploy_contrail_command.sh
+++ contrail-command/files/deploy_contrail_command.sh
@@ -1,6 +1,14 @@
 #!/bin/bash -e
 # The script is needed to run Contrail Command deploy in virtualenv
-# to prevent issues with installed librarys
+# to prevent issues with installed libraries
+
+if [ -f /etc/default/locale ]; then
+  # if system has locale then we have to export it to pip run below
+  # otherwise 'pip install' may fail on non-ascii chars
+  set -o allexport
+  source /etc/default/locale || /bin/true
+  set +o allexport
+fi
 
 apt-get install -y python3-pip python3-virtualenv
 
