fdcb184239f3854aa9094a7a2b42dd58dd2d2704
tikitavi <akravets@juniper.net>
2022-02-03 18:13:37 +0300

    Fix probable issue with executable path in systemd services
    add absolute path to ctr command
    
    Closes-Jira-bug: CEM-25835
    Signed-off-by: tikitavi <akravets@juniper.net>
    Change-Id: I5cb10bc003df6913482d4b9ea77611aa0c74954f

--- contrail-controller/templates/service_stop.sh
+++ contrail-controller/templates/service_stop.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
-if ctr task ls | grep {{ name }} | grep RUNNING ; then
-    ctr task kill -s {{ kill_signal or 'SIGQUIT' }} {{ name }}
+if /usr/bin/ctr task ls | grep {{ name }} | grep RUNNING ; then
+    /usr/bin/ctr task kill -s {{ kill_signal or 'SIGQUIT' }} {{ name }}
     # wait for exited code for service. Each 1 second, max wait 6 seconds
     for i in {1..6} ; do
         sleep 1
-        state=$(ctr task list | grep {{ name }} | awk '{print$3}')
+        state=$(/usr/bin/ctr task list | grep {{ name }} | awk '{print$3}')
         if [[ -z $state || $state != 'RUNNING' ]] ; then
             break
         fi
@@ -16,7 +16,7 @@ if ctr task ls | grep {{ name }} | grep RUNNING ; then
 {%- endif %}
     done
 fi
-if ctr task ls | grep {{ name }} | grep RUNNING ; then
-    ctr task kill -s SIGKILL {{ name }}
+if /usr/bin/ctr task ls | grep {{ name }} | grep RUNNING ; then
+    /usr/bin/ctr task kill -s SIGKILL {{ name }}
     sleep 1
 fi
