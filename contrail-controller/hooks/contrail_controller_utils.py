241b7a1f15d5bde4bd4c37a75fc9eb5158f7cbe3
tikitavi <akravets@juniper.net>
2022-12-16 17:24:02 +0300

    add reaper-enabled option
    Signed-off-by: tikitavi <akravets@juniper.net>
    Change-Id: If96d2ab68755a625636ab9e0992d715911a43e44

--- contrail-controller/hooks/contrail_controller_utils.py
+++ contrail-controller/hooks/contrail_controller_utils.py
@@ -150,6 +150,7 @@ def get_context():
     ctx["apply_defaults"] = config.get("apply-defaults")
     ctx["huge_scale"] = config.get("huge-scale", False)
     ctx["container_runtime"] = config.get("container_runtime")
+    ctx["reaper_enabled"] = config.get("reaper-enabled")
     ctx.update(common_utils.json_loads(config.get("orchestrator_info"), dict()))
     if not ctx.get("cloud_orchestrators"):
         ctx["cloud_orchestrators"] = [ctx.get("cloud_orchestrator")] if ctx.get("cloud_orchestrator") else list()
